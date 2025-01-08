efdccbd2ed9fdc44c456adeb1147c335f1f792d9
Ahmet Öztürk Şahin <83590669+ahmetsahn@users.noreply.github.com>
2025-01-08 17:31:25 +0300

    updated length and width buffs

--- Assets/Scripts/Runtime/Gameplay/Hand/Controller/HandParentLengthBuffController.cs
+++ Assets/Scripts/Runtime/Gameplay/Hand/Controller/HandParentLengthBuffController.cs
@@ -5,86 +5,107 @@ using Runtime.Gameplay.Hand.View;
 using Runtime.Signals;
 using UnityEngine;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace Runtime.Gameplay.Hand.Controller
 {
     public class HandParentLengthBuffController : IDisposable
     {
         private readonly HandParentView _view;
-        
         private readonly HandParentModel _model;
-        
         private readonly SignalBus _signalBus;
-        
         private readonly IInstantiator _instantiator;
-        
+
         public HandParentLengthBuffController(HandParentView view, HandParentModel model, SignalBus signalBus, IInstantiator instantiator)
         {
             _view = view;
             _model = model;
             _signalBus = signalBus;
             _instantiator = instantiator;
-            
-            SubscribeEvents();
+
+            _signalBus.Subscribe<LengthBuffSignal>(HandleLengthBuff);
         }
-        
-        private void SubscribeEvents()
+
+        private void HandleLengthBuff(LengthBuffSignal signal)
         {
-            _signalBus.Subscribe<LengthBuffSignal>(OnLengthBuff);
+            int buffValue = signal.BuffValue;
+            
+            if (buffValue > 0)
+                AddHands(buffValue);
+            else if (buffValue < 0)
+                RemoveHands(-buffValue);
+
+            _model.CurrentLength = Mathf.Clamp(_model.CurrentLength, 1, _model.MaxLength);
         }
-        
-        private void OnLengthBuff(LengthBuffSignal signal)
+
+        private void AddHands(int buffValue)
         {
-            int count = signal.BuffValue;
-            
-            if (count + _model.CurrentLength > _model.MaxLength)
+            int availableSpace = _model.MaxLength - _model.CurrentLength;
+            int countToAdd = Mathf.Min(buffValue, availableSpace);
+
+            for (int i = _model.CurrentLength; i < _model.CurrentLength + countToAdd; i++)
             {
-                count = _model.MaxLength - _model.CurrentLength;
+                CreateHandsAtIndex(i);
+                UpdateHandPositionsAtIndex(i);
             }
-            
-            for(int i = _model.CurrentLength; i < _model.CurrentLength + count; i++)
-            {
-                GenerateHands(i);
-                UpdateHandPositions(i);
-            }
-            
-            _model.CurrentLength += signal.BuffValue;
-            
-            if(_model.CurrentLength > _model.MaxLength)
-            {
-                _model.CurrentLength = _model.MaxLength;
-            }
+
+            _model.CurrentLength += countToAdd;
         }
-        
-        private void GenerateHands(int index)
+
+        private void RemoveHands(int buffValue)
         {
+            int removableCount = Mathf.Min(buffValue, _model.CurrentLength - 1);
+
+            for (int i = _model.CurrentLength - 1; i >= _model.CurrentLength - removableCount; i--)
+            {
+                DestroyHandsAtIndex(i);
+            }
+
+            _model.CurrentLength -= removableCount;
+        }
+
+        private void CreateHandsAtIndex(int index)
+        {
+            if (index < 0 || index >= _view.LineTransforms.Length) return;
+
+            Transform lineTransform = _view.LineTransforms[index];
             for (int i = 0; i < _model.CurrentWidth; i++)
             {
-                _instantiator.InstantiatePrefab(_model.HandPrefab, _view.LineTransforms[index]);
+                _instantiator.InstantiatePrefab(_model.HandPrefab, lineTransform);
             }
         }
-        
-        private void UpdateHandPositions(int index)
-        {
-            int count = _view.LineTransforms[index].childCount;
-            float centerOffset = (count - 1) * _model.DistanceBetweenHands / 2;
 
-            for (int i = 0; i < _view.LineTransforms[index].childCount; i++)
+        private void DestroyHandsAtIndex(int index)
+        {
+            if (index < 1 || index >= _view.LineTransforms.Length) return;
+
+            Transform lineTransform = _view.LineTransforms[index];
+            for (int i = lineTransform.childCount - 1; i >= 0; i--)
+            {
+                Object.Destroy(lineTransform.GetChild(i).gameObject);
+            }
+        }
+
+        private void UpdateHandPositionsAtIndex(int index)
+        {
+            if (index < 0 || index >= _view.LineTransforms.Length) return;
+
+            Transform lineTransform = _view.LineTransforms[index];
+            int childCount = lineTransform.childCount;
+            float centerOffset = (childCount - 1) * _model.DistanceBetweenHands / 2;
+
+            for (int i = 0; i < childCount; i++)
             {
                 float targetX = (i * _model.DistanceBetweenHands) - centerOffset;
-                Vector3 targetLocalPosition = new Vector3(targetX, _view.LineTransforms[index].GetChild(0).localPosition.y, 0);
-                
-                _view.LineTransforms[index].GetChild(i).DOLocalMove(targetLocalPosition, _model.ChildHandMoveDuration);
+                Vector3 targetLocalPosition = new Vector3(targetX, lineTransform.GetChild(0).localPosition.y, 0);
+
+                lineTransform.GetChild(i).DOLocalMove(targetLocalPosition, _model.ChildHandMoveDuration);
             }
         }
-        
-        private void UnsubscribeEvents()
-        {
-            _signalBus.Unsubscribe<LengthBuffSignal>(OnLengthBuff);
-        }
+
         public void Dispose()
         {
-            UnsubscribeEvents();
+            _signalBus.Unsubscribe<LengthBuffSignal>(HandleLengthBuff);
         }
     }
 }
--- Assets/Scripts/Runtime/Gameplay/Hand/Controller/HandParentWidthBuffController.cs
+++ Assets/Scripts/Runtime/Gameplay/Hand/Controller/HandParentWidthBuffController.cs
@@ -1,87 +1,109 @@
 ﻿using System;
+using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using Runtime.Gameplay.Hand.Model;
 using Runtime.Gameplay.Hand.View;
 using Runtime.Signals;
 using UnityEngine;
 using Zenject;
+using Object = UnityEngine.Object;
 
 namespace Runtime.Gameplay.Hand.Controller
 {
     public class HandParentWidthBuffController : IDisposable
     {
         private readonly HandParentView _view;
-        
         private readonly HandParentModel _model;
-        
         private readonly SignalBus _signalBus;
-
         private readonly IInstantiator _instantiator;
-        
+
         public HandParentWidthBuffController(HandParentView view, HandParentModel model, SignalBus signalBus, IInstantiator instantiator)
         {
             _view = view;
             _model = model;
             _signalBus = signalBus;
             _instantiator = instantiator;
-            
+
             SubscribeEvents();
         }
-        
-        
+
         private void SubscribeEvents()
         {
             _signalBus.Subscribe<WidthBuffSignal>(OnWidthBuff);
         }
-        
-        private void OnWidthBuff(WidthBuffSignal signal)
+
+        private async void OnWidthBuff(WidthBuffSignal signal)
         {
-            if(_model.CurrentWidth > _model.MaxWidth)
+            ClampModelWidth();
+
+            int iterations = Mathf.Abs(signal.BuffValue);
+            bool isBuff = signal.BuffValue > 0;
+
+            for (int i = 0; i < _model.CurrentLength; i++)
             {
-                _model.CurrentWidth = _model.MaxWidth;
-            }
-            
-            for(int i = 0; i<_model.CurrentLength; i++)
-            {
-                GenerateHands(signal.BuffValue, i);
+                for (int j = 0; j < iterations; j++)
+                {
+                    if (isBuff)
+                        AddHand(i);
+                    else
+                        RemoveHand(i);
+
+                    await UniTask.Delay(TimeSpan.FromSeconds(0.05f));
+                }
+
                 UpdateHandPositions(i);
             }
-            
-            _model.CurrentWidth += signal.BuffValue;
-        }
-        
-        private void GenerateHands(int count, int index)
-        {
-            if (count + _model.CurrentWidth > _model.MaxWidth)
-            {
-                count = _model.MaxWidth - _model.CurrentWidth;
-            }
 
-            for (int i = 0; i < count; i++)
-            {
-                _instantiator.InstantiatePrefab(_model.HandPrefab, _view.LineTransforms[index]);
-            }
+            AdjustModelWidth(signal.BuffValue);
         }
-        
-        private void UpdateHandPositions(int index)
-        {
-            int count = _view.LineTransforms[index].childCount;
-            float centerOffset = (count - 1) * _model.DistanceBetweenHands / 2;
 
-            for (int i = 0; i < _view.LineTransforms[index].childCount; i++)
+        private void ClampModelWidth()
+        {
+            _model.CurrentWidth = Mathf.Clamp(_model.CurrentWidth, 1, _model.MaxWidth);
+        }
+
+        private void AddHand(int lineIndex)
+        {
+            if (_model.CurrentWidth >= _model.MaxWidth) return;
+
+            _instantiator.InstantiatePrefab(_model.HandPrefab, _view.LineTransforms[lineIndex]);
+        }
+
+        private void RemoveHand(int lineIndex)
+        {
+            var lineTransform = _view.LineTransforms[lineIndex];
+            if (lineTransform.childCount > 1)
             {
-                float targetX = (i * _model.DistanceBetweenHands) - centerOffset;
-                Vector3 targetLocalPosition = new Vector3(targetX, _view.LineTransforms[index].GetChild(0).localPosition.y, 0);
-                
-                _view.LineTransforms[index].GetChild(i).DOLocalMove(targetLocalPosition, _model.ChildHandMoveDuration);
+                var lastChild = lineTransform.GetChild(lineTransform.childCount - 1);
+                Object.Destroy(lastChild.gameObject);
             }
         }
-        
+
+        private void UpdateHandPositions(int lineIndex)
+        {
+            var lineTransform = _view.LineTransforms[lineIndex];
+            int childCount = lineTransform.childCount;
+            float centerOffset = (childCount - 1) * _model.DistanceBetweenHands / 2;
+
+            for (int i = 0; i < childCount; i++)
+            {
+                float targetX = i * _model.DistanceBetweenHands - centerOffset;
+                Vector3 targetLocalPosition = new Vector3(targetX, lineTransform.GetChild(0).localPosition.y, 0);
+
+                lineTransform.GetChild(i).DOLocalMove(targetLocalPosition, _model.ChildHandMoveDuration);
+            }
+        }
+
+        private void AdjustModelWidth(int buffValue)
+        {
+            _model.CurrentWidth = Mathf.Clamp(_model.CurrentWidth + buffValue, 1, _model.MaxWidth);
+        }
+
         private void UnsubscribeEvents()
         {
             _signalBus.Unsubscribe<WidthBuffSignal>(OnWidthBuff);
         }
-        
+
         public void Dispose()
         {
             UnsubscribeEvents();
